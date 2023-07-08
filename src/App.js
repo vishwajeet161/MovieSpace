3470025e39fc9a92ac3c548083c4a44ee07ef505
Vishwajeet161 <vishwajeet.patel161@gmail.com>
2023-07-09 01:15:22 +0530

    Adding complete project

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect }  from "react";
+import MovieCard from './MovieCard';
+ //c032e2d7
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
+
+// const movie1 = {
+//     "Title": "The Amazing Spiderman 2 Webb Cut",
+//     "Year": "2021",
+//     "imdbID": "tt18351128",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
+// };
+
+const App = () => {
+
+    const [movies, setMovies] =  useState([]);
+    const [searchTerm, setSearchterm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []);
+
+    return(
+        <div className="app">
+            <h1>MovieSpace</h1>
+
+            <div className="search">\
+                <input 
+                    placeholder="Search for movies"
+                    value = {searchTerm}
+                    onChange={(e) => setSearchterm(e.target.value)}
+                />
+                <img
+                    src = {SearchIcon}
+                    alt = "search"
+                    onClick = {() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {
+                movies?.length >0
+                    ?(
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie = {movie}/>
+                        ))}
+                    </div>
+                    ): (
+                        <div className="empty">
+                            <h2>No movies Found</h2>
+                        </div>
+                    )
+            }
+           
+        </div>
+    );
+}
+
+export default App;
